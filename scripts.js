b2cde3f876a2288cabd55b56701dfca80c98b673
Shogo Hardy <shogo.a.hardy@gmail.com>
2024-11-28 21:36:38 -0700

    Fixed sidebar home page layout

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 /* Sidebar */
 const sidebar = document.querySelector('#sidebar');
 const sidebarToggler = document.querySelector('.sidebar_toggler');
-
+const logo = document.getElementById("logo");
+const line = document.getElementById("line");
+const images = ['Images/LogoClose.PNG', 'Images/LogoOpen.PNG'];
+const lineColor = ["black", "white"];
+let currentIndex = 0;
 
 // Toggling the Sidebar
 sidebarToggler.addEventListener('click', () => {
     sidebar.classList.toggle('show');
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    logo.src = images[currentIndex];
+    line.style.borderColor = lineColor[currentIndex];
 });
 
 
@@ -13,5 +20,8 @@ sidebarToggler.addEventListener('click', () => {
 window.addEventListener('click', (e) => {
     if (e.target.id !== 'sidebar' && e.target.className !== 'sidebar_toggler') {
         sidebar.classList.remove('show');
+        logo.src = "Images/LogoClose.PNG";
+        line.style.borderColor = "black";
+        currentIndex = 0;
     }
 });
