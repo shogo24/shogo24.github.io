13cf8e4afa6eb54aa1ecc3a40e0c01a62ce7e96e
Shogo Hardy <shogo.a.hardy@gmail.com>
2024-12-17 18:00:50 -0700

    added resume and cheat sheets

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-/* Sidebar */
+// Sidebar
 const sidebar = document.querySelector('#sidebar');
 const sidebarToggler = document.querySelector('.sidebar_toggler');
 const logo = document.getElementById("logo");
@@ -25,3 +25,25 @@ window.addEventListener('click', (e) => {
         currentIndex = 0;
     }
 });
+
+
+// Function to update the favicon based on the current color scheme
+function updateFavicon() {
+    const favicon = document.getElementById('favicon');
+    
+    // Check the current color scheme (light or dark)
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      // If dark mode, set the dark mode favicon
+      favicon.href = 'Images/favconDark.PNG';
+    } else {
+      // If light mode, set the light mode favicon
+      favicon.href = 'Images/favconLight.PNG';
+    }
+  }
+  
+  // Call the function to set the favicon when the page loads
+  updateFavicon();
+  
+  // Listen for changes in the color scheme and update the favicon accordingly
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateFavicon);
+  
